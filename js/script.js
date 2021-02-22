0a27fdad71f5f0322b772d471748d069a6e36b42
LAPTOP-6CF0ICQH\stevi <stevillis@hotmail.com>
2021-02-21 22:42:25 -0400

    Netflix Home Clone

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function watch() {
+    window.open('https://www.youtube.com/watch?v=xOsLIiBStEs', '_blank');
+}
+
+function info() {
+    window.location.href = 'https://pt.wikipedia.org/wiki/Soul_(filme)';
+}
+
